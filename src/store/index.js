0917541b044836b711532e500de097f22c8ef937
zerrorTwo <namle022004@gmail.com>
2025-04-25 15:00:42 +0700

    authen and author

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
-import authSlice from './slices/authSlice'; // Import authSlice
+import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { apiSlice } from './apis/apiSlice';
-import { configureStore } from '@reduxjs/toolkit'
-import paginationSlice from './slices/paginationSlice'
-import { heritageApi } from './apis/heritageApi'
-import { leaderboardApi } from './apis/leaderboardApi'
-import { knowledgeTestApi } from './apis/knowledgeTestApi'
+import { heritageApi } from './apis/heritageApi';
+import { leaderboardApi } from './apis/leaderboardApi';
+import { knowledgeTestApi } from './apis/knowledgeTestApi';
+import authSlice from './slices/authSlice';
+import paginationSlice from './slices/paginationSlice';
+import { chatApi } from './apis/chatSlice';
 
 export const store = configureStore({
   reducer: {
@@ -13,11 +14,18 @@ export const store = configureStore({
     auth: authSlice,
     [heritageApi.reducerPath]: heritageApi.reducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
+    [chatApi.reducerPath]: chatApi.reducer,
     [leaderboardApi.reducerPath]: leaderboardApi.reducer,
     [knowledgeTestApi.reducerPath]: knowledgeTestApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => 
-    getDefaultMiddleware().concat(heritageApi.middleware, leaderboardApi.middleware, knowledgeTestApi.middleware, apiSlice.middleware),
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      heritageApi.middleware,
+      leaderboardApi.middleware,
+      knowledgeTestApi.middleware,
+      apiSlice.middleware,
+      chatApi.middleware // Added chatApi middleware
+    ),
+});
 
 setupListeners(store.dispatch);
