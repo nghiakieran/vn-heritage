cada597219ea4b3e5468615db917bd46f1840a65
phanvanthuan06052004 <phanthuan652004@gmail.com>
2025-05-06 09:27:36 +0700

    fix: handle images to introduce

--- src/pages/About/data/teamMembersData.js
+++ src/pages/About/data/teamMembersData.js
@@ -3,7 +3,7 @@ export const teamMembers = [
     name: 'Nguyễn An Thành Phát',
     role: 'Founder & Leader',
     bio: 'Với hơn 3 năm kinh nghiệm trong lĩnh vực phát triển website.',
-    img: '/images/profile.jpg',
+    img: '/images/profile-phat.jpg',
     social: {
       facebook: 'https://www.facebook.com/',
       linkedin: 'https://www.linkedin.com/'
@@ -13,7 +13,7 @@ export const teamMembers = [
     name: 'Lê Quốc Nam',
     role: 'Backend Developer',
     bio: 'Chuyên gia công nghệ với kinh nghiệm phát triển backend với các nền tảng tương tác và ứng dụng AI vào sản phẩm.',
-    img: '/images/profile.jpg',
+    img: '/images/profile-nam.jpg',
     social: {
       facebook: 'https://www.facebook.com/',
       linkedin: 'https://www.linkedin.com/'
@@ -23,7 +23,7 @@ export const teamMembers = [
     name: 'Phan Văn Thuận',
     role: 'Backend Developer',
     bio: 'Nhà nghiên cứu công nghệ với kinh nghiệm thiết kế database mạnh mẽ, sử dụng các công nghệ hiện đại.',
-    img: '/images/profile.jpg',
+    img: '/images/profile-thuan.png',
     social: {
       facebook: 'https://www.facebook.com/',
       linkedin: 'https://www.linkedin.com/'
@@ -33,7 +33,7 @@ export const teamMembers = [
     name: 'Lê Chí Nghĩa',
     role: 'Frontend Developer',
     bio: 'Nhà thiết kế với niềm đam mê tạo ra trải nghiệm người dùng trực quan và hấp dẫn.',
-    img: '/images/profile.jpg',
+    img: '/images/profile-nghia.jpg',
     social: {
       facebook: 'https://www.facebook.com/le.chi.nghia.621880/',
       linkedin: 'https://www.linkedin.com/in/nghia-le-1b3bb32b8/'
@@ -43,7 +43,7 @@ export const teamMembers = [
     name: 'Nguyễn Thanh Hùng',
     role: 'AI Specialist',
     bio: 'Chuyên gia AI với kinh nghiệm phát triển các hệ thống trợ lý ảo thông minh.',
-    img: '/images/profile.jpg',
+    img: '/images/profile-hung.jpg',
     social: {
       facebook: 'https://www.facebook.com/',
       linkedin: 'https://www.linkedin.com/'
